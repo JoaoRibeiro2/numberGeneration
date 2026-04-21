5ed873b6d3dee16c70704043ddc9b590bec328ec
Joao Vitor <joaovitorsilvaribeiro1006@gmail.com>
2026-04-21 11:04:36 -0300

    Excluído arquivos desnecessarios

--- js/modules/numberGenerator.js
+++ js/modules/numberGenerator.js
@@ -1,17 +1,17 @@
 
-export function numberGenerator(interval, min, max, selectOption){
+export function numberGenerator(quantify, min, max, selectOption){
   const array = []
 
   if(min>max){
     alert("Primeiro valor deve ser menor que o segundo")
     location.reload()
-  }else if(interval > (max-min+1)){
+  }else if(quantify > (max-min+1)){
     alert("O número do sorteio é maior que a quantidade de números disponíveis")
     location.reload()
   }{ 
       switch(selectOption){
       case "repeat":
-      while(array.length < interval){
+      while(array.length < quantify){
       const number = Math.floor(Math.random() * (max - min + 1)) + min
       array.push(number)
       }
@@ -19,7 +19,7 @@ export function numberGenerator(interval, min, max, selectOption){
     break
     case "notRepeat":
     
-    while(array.length < interval){
+    while(array.length < quantify){
         const number = Math.floor(Math.random() * (max - min + 1)) + min
         if(!array.includes(number)){
           array.push(number)
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const h3 = document.querySelector(".h3-form")
 const pForm = document.querySelector(".p-form")
 
 const numbersInput = document.querySelectorAll(".numbers")
-const interval = document.querySelector("#numbers")
+const quantify = document.querySelector("#quantify")
 const min = document.querySelector("#min")
 const max = document.querySelector("#max")
 const repeat = document.querySelector("#repeat")
@@ -41,7 +41,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault()
  
 
-  let interval_number = Number(interval.value)
+  let quantify_number = Number(quantify.value)
   let min_number = Number(min.value)
   let max_number = Number(max.value)
   let valueSelected 
@@ -54,7 +54,7 @@ form.addEventListener("submit", (e) => {
 
   form.classList.add("hidden")
   formDiv.classList.add("format-result")
-  let result = numberGenerator(interval_number, min_number, max_number, valueSelected)
+  let result = numberGenerator(quantify_number, min_number, max_number, valueSelected)
   resultDiv.classList.remove("hidden")
   formatResult(h3, pForm)
   insertNumbers(result, spansGroup)
@@ -71,7 +71,7 @@ form.addEventListener("submit", (e) => {
     spans.forEach((span) => {
       span.remove()
     })
-      result = numberGenerator(interval_number, min_number, max_number, valueSelected)
+      result = numberGenerator(quantify_number, min_number, max_number, valueSelected)
       insertNumbers(result, spansGroup)
   }
   button2.onclick = () => {
